6a66800bb4cee8bff82d73e01c88ca377c20acac
cordelia <cordeliasixth@gmail.com>
2021-06-20 19:55:55 +0900

    add coments

--- script.js
+++ script.js
@@ -4,19 +4,31 @@ const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
-// 新年の日時
-const target = '2021 6 21';
+//  目標としたい日時
+const target = '2021 6 23';
 
 function countDown() {
+  // 目標日時
   const targetDate = new Date(target);
+  // 現在
   const currentDate = new Date();
 
   // 残りの時間を計算
+  // ミリ秒を秒になおす
   const totalSeconds = (targetDate - currentDate) / 1000;
 
+  console.log(totalSeconds);
+
+  // 日 = 残り時間(秒) / 3600秒(1時間)ここで時間の単位に変換している / 24時間(1日)
   const days = Math.floor(totalSeconds / 3600 / 24);
+
+  // 時間　= (残り時間(秒) / 3600秒(1時間) を24時間から溢れたものが残り時間
   const hours = Math.floor(totalSeconds / 3600) % 24;
+  
+  // 分 = (残り時間(秒) / 60秒(1分)ここで分の単位に変換している) を60分(1時間)の中から溢れたものが残りの分
   const mins = Math.floor(totalSeconds / 60) % 60;
+
+  // 秒 = 60秒から溢れたものが残りの秒
   const seconds = Math.floor(totalSeconds % 60);
 
   daysEl.innerHTML = days;

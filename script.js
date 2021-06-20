6a32bfcfc747ca178eae642d85f5d69d6abc642f
cordelia <cordeliasixth@gmail.com>
2021-06-20 18:19:10 +0900

    modify

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
 // 新年の日時
-const newYears = '2021 6 21';
+const target = '2021 6 21';
 
 function countDown() {
-  const newYearsDate = new Date(newYears);
+  const targetDate = new Date(target);
   const currentData = new Date();
 
   // 残りの時間を計算
-  const totalSeconds = (newYearsDate - currentData) / 1000;
+  const totalSeconds = (targetDate - currentData) / 1000;
 
   const days = Math.floor(totalSeconds / 3600 / 24);
   const hours = Math.floor(totalSeconds / 3600) % 24;
